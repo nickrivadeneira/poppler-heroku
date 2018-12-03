dc09ffb9ec1e6628a33484cbb88eb0b9449f9140
Nick Rivadeneira <nrivadeneira@vericred.com>
2018-12-03 14:25:34 -0500

    GObjectIntrospection

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-puts ENV['GI_TYPELIB_PATH']
+puts "GI_TYPELIB_PATH: #{ENV['GI_TYPELIB_PATH']}"
+require 'gobject-introspection'
+puts ".search_path: #{GObjectIntrospection::Repository.search_path}"
+GObjectIntrospection.load('Poppler')
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
